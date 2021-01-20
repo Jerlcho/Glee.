2c7e5b8488362ae387078019ef4accd848c29c5d
Jerlcho <jdm_9407@inbox.ru>
2021-01-20 11:32:25 +0900

    added footer and slider

--- gulpfile.js
+++ gulpfile.js
@@ -40,6 +40,7 @@ function images() {
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
+    'node_modules/slick-carousel/slick/slick.js',
     'app/js/script.js'
   ])
     .pipe(concat('script.min.js'))
